229224f3aee8b5b8e450a46fee388a7dc1f09713
coremand <valinevite@gmail.com>
2023-11-01 17:58:44 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Button from './Button';
+import { GoBell } from "react-icons/go"
 
-function App() {
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div>
+            <Button primary>
+                <GoBell />
+                Buy Now!
+            </Button>
+        </div>
+        <div>
+            <Button>Search</Button>
+        </div>
+        <div>
+            <Button outline primary>Login</Button>
+        </div>
+        <div>
+            <Button outline secondary>Checkout</Button>
+        </div>
+        <div>
+            <Button danger>Danger</Button>
+        </div>
     </div>
-  );
+  )
 }
-
-export default App;
